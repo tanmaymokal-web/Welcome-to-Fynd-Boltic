952c3a1c8c0ea03aa6ed4d8eea9c5dda02c93f6e
tanmaymokal-web <tanmaymokal@gofynd.com>
2026-04-21 11:34:02 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('mainnd github connected ho gaya 13thhh updateee Hello World');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
